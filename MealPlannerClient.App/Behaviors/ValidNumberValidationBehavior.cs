98ca74385903f293890fede198cfb86a93639171
Denis Kaminsky <kkaminsky.denis@gmail.com>
2024-10-17 22:15:01 +0200

    added input validation

---
+++ MealPlannerClient.App/Behaviors/ValidNumberValidationBehavior.cs
@@ -0,0 +1,33 @@
+﻿namespace MealPlannerClient.App.Behaviors
+{
+    public class PositiveNumberValidationBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += Bindable_TextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= Bindable_TextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void Bindable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.NewTextValue))
+            {
+                var isValidDouble = double.TryParse(e.NewTextValue, out double value) && value >= 0;
+                if (!isValidDouble)
+                {
+                    ((Entry)sender).Text = e.OldTextValue;
+                }
+            }
+            else
+            {
+                ((Entry)sender).Text = null;
+            }
+        }
+    }
+}
